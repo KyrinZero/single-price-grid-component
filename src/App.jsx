aebb1a68635bb410502eb1165f1acfd1437a521d
Bayajit Mohammed <bayajitmohammed@gmail.com>
2021-01-20 00:55:58 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
       <footer>
         <p class="attribution">
           Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">
             Frontend Mentor
           </a>
           . Coded by <a href="https://github.com/KyrinZero/single-price-grid-component">Bayajit Mohammed</a>.
